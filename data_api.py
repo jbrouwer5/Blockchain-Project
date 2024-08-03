7e405169d52f4cb1e4c2ddfef639bc453acc3045
jbrouwer5 <jbrouwer@uchicago.edu>
2024-08-03 13:32:31 +0200

    Mocked data and api call

---
+++ data_api.py
@@ -0,0 +1,19 @@
+import pandas as pd 
+
+def load_data(file_path):
+    df = pd.read_csv(file_path) 
+    return df
+
+def query_data(id, data):
+    return data.iloc[id]
+
+if __name__ == "__main__":
+    file_path = "MOCK_DATA.csv"
+    data = load_data(file_path)
+    row = query_data(5, data)
+    print(row)
+
+
+
+
+
