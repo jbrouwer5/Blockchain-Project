c3c7f4a116f38162bbc13f1659c7172aaeea274c
jbrouwer5 <jbrouwer@uchicago.edu>
2024-08-06 13:34:13 +0200

    Updated data retreival functions

--- data_api.py
+++ data_api.py
@@ -7,11 +7,15 @@ def load_data(file_path):
 def query_data(id, data):
     return data.iloc[id]
 
+# verified_authority_index is 1 or 2
+# patient id ranges from 0-999 for both
+def get_patient_data(verified_authority_index, patient_id):
+    data = load_data("VA" + str(verified_authority_index) + "_MOCK_DATA.csv")
+    patient_data = query_data(patient_id, data)
+    return patient_data
+    
 if __name__ == "__main__":
-    file_path = "MOCK_DATA.csv"
-    data = load_data(file_path)
-    row = query_data(5, data)
-    print(row)
+    print(get_patient_data(1, 10))
 
 
 
